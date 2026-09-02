552f1b9251218a6988a1c9bfc5b2b683a81d526d
Gabriel Vicente <vicente.gabrielv@gmail.com>
2026-09-02 14:27:12 +0000

    adiciona error handling para fechar as conexões do banco

--- infra/database.js
+++ infra/database.js
@@ -9,9 +9,15 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
+
+  try {
   const result = await client.query(queryObject);
-  await client.end();
   return result;
+  } catch (error) {
+    console.error(error);
+  }finally {
+    await client.end();
+  }
 }
 
 export default {
